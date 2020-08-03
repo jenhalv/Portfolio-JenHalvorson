18e7ba716f39676c411a1a12b4e92936db4b3570
Jen <jenhalv06@gmail.com>
2020-08-03 17:16:25 -0500

    Initial StartUp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
