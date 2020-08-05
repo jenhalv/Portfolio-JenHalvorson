71d182df6d813f6c87ef51e6b7fc1014d80e8488
Jen <jenhalv06@gmail.com>
2020-08-04 21:31:21 -0500

    Ed & footer updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
