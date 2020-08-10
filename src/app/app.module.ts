950c72787e020701974c4bab20ef0190434e6273
Jen <jenhalv06@gmail.com>
2020-08-10 09:57:26 -0500

    updated changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { EducationComponent } from './education/education.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BriefDescriptionComponent } from './brief-description/brief-description.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     ToolbarComponent,
-    EducationComponent
+    EducationComponent,
+    LandingPageComponent,
+    BriefDescriptionComponent,
+    ContactMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
