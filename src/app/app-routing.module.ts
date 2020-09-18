9abfb2383f866aee1a54960e87984c1279787aec
Jen <jenhalv06@gmail.com>
2020-09-18 11:02:44 -0500

    Picture Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EducationComponent } from './about/education/education.component';
-import { WorkComponent } from './about/work/work.component';
-import { SkillsComponent } from './about/skills/skills.component';
-import { AppComponent } from './app.component';
 
-
-const routes: Routes = [
-  { path: 'education', component: EducationComponent },
-  { path: 'work', component: WorkComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: '', component: AppComponent},
- ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule],
   exports: [RouterModule],
   providers: []
 })
