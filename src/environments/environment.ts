f0e3e18843b9c63c8d72b57d8b8a81734233cdc0
Jen <jenhalv06@gmail.com>
2020-12-08 19:22:45 -0600

    Firebase Addition

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,18 @@ export const environment = {
   production: false
 };
 
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDTnpUVTAurFuryH5qT1WUyXfjY1wwBZKo',
+  authDomain: 'portfolio-jenhalvorson.firebaseapp.com',
+  databaseURL: 'https://portfolio-jenhalvorson.firebaseio.com',
+  projectId: 'portfolio-jenhalvorson',
+  storageBucket: 'portfolio-jenhalvorson.appspot.com',
+  messagingSenderId: '353200162414',
+  appId: '1:353200162414:web:f8176952ee08d6b69c087f',
+  measurementId: 'G-53YE5C8DMZ'
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
