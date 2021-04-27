8debcc98260c623cb10e42bc4f1951afa182de72
Jen <jenhalv06@gmail.com>
2021-04-27 18:28:05 -0500

    Education Card

--- src/app/about/education/education.component.ts
+++ src/app/about/education/education.component.ts
@@ -6,6 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent implements OnInit {
+  CARDS = [
+      {
+        image: 'assets/education-icons/ngGirls.png',
+        imageAlt: 'ngGirls',
+        website: 'https://workshops.ng-girls.org/',
+        },
+      {
+        image: 'assets/education-icons/KCWIT1.png',
+        imageAlt: 'Kansas City Women in Tech',
+        website: 'https://kcwomenintech.org/',
+        },
+      {
+        image: 'assets/education-icons/CodeA.png',
+        imageAlt: 'Codecademy',
+        website: 'https://www.codecademy.com/',
+        },
+      {
+        image: 'assets/education-icons/FCC.png',
+        imageAlt: 'Free Code Camp',
+        website: 'https://www.freecodecamp.org/',
+        },
+      {
+        image: 'assets/education-icons/PS.png',
+        imageAlt: 'PluralSight',
+        website: 'https://www.pluralsight.com/'
+        },
+      {
+        image: 'assets/education-icons/UD.png',
+        imageAlt: 'Udemy',
+        website: 'https://www.udemy.com/'
+        },
+      {
+        image: 'assets/education-icons/KCDC.png',
+        imageAlt: 'Kansas City Developers Conference 2019',
+        website: 'https://kcdc2019.myconf.app/',
+        },
+      {
+        image: 'assets/education-icons/CA.png',
+        imageAlt: 'Claim Academy',
+        website: 'https://claimacademystl.com/',
+        },
+  ];
 
   constructor() { }
 
