b32fed889b374e32d0ebb7e680ec0792ab156075
Jennifer Halvorson <jenhalv06@gmail.com>
2021-05-27 16:38:03 -0500

    A11Y changes

--- src/app/about/skills/skills.component.ts
+++ src/app/about/skills/skills.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-  bufferValue = 100;
 
   SKILLS = [
     {
